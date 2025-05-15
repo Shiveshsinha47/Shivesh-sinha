2ee9d9e30f68e419a075366f0a946cbe9b1b9768
Shivesh sinha <164620117+Shiveshsinha47@users.noreply.github.com>
2025-05-15 16:30:37 +0530

    Add files via upload

---
+++ pb2.py
@@ -0,0 +1,138 @@
+# s=set()
+# n=input("Enter the number:")
+# s.add(int(n))
+# n=input("Enter the number:")
+# s.add(int(n))
+# n=input("Enter the number:")
+# s.add(int(n))
+# n=input("Enter the number:")
+# s.add(int(n))
+# n=input("Enter the number:")
+# s.add(int(n))
+# n=input("Enter the number:")
+# s.add(int(n))
+# n=input("Enter the number:")
+# s.add(int(n))
+# n=input("Enter the number:")
+# s.add(int(n))
+# print(s)
+
+#area of traingle and perimeter
+# def area():
+#     l=float(input("Enter the num:"))
+#     b=float(input("Enter the num:"))
+#     h=float(input("Enter the num:"))
+#     area=(1/2)*l*b*h
+#     print("Area of triangle is",area)
+# def perimeter():
+#     l=float(input("Enter the num:"))
+#     b=float(input("Enter the num:"))
+#     h=float(input("Enter the num:"))
+#     perimeter=2*(l+b+h)
+#     print("Perimeter of triangle is",perimeter)
+# area()
+# perimeter()
+
+
+# list=[1,2,3,4,5]
+# tuple=(1,2,3,4,5)
+# set={1,2,3,4,5}
+# dict={"name":"SHIVESH","age":20,"city":"Delhi"}
+# print("List:",list)
+# print("Tuple:",tuple)
+# print("Set:",set)
+# print("Dictionary:",dict)
+# print("List:",list[0])
+# print("List:",list.pop(1))
+# print("Tuple:",tuple[0])
+# print("Set:",set.pop())
+# print("Dictionary:",dict["name"])
+# print("Dictionary:",dict["age"])
+# print("Dictionary:",dict["city"])
+# print("List:",list[0:3])
+# print("List:",list[0:3:2])
+# print("Set:", set.union({1, 6, 7}))
+# l = [1, 2, 3, 4, 5, 6, 7]
+# for i in l:
+#     print(i)
+# else:
+#     print("end")
+
+# for i in range(80):
+#     if i == 56:
+#         continue
+#     print(i)
+
+# for i in range(80):
+#     if i==3:
+#         break
+#     print(i)
+        
+# l=[1,2,3,4]
+# for i in l:
+#     pass
+
+# item = int(input("Enter the num: "))
+# print(f"Multiplication table of {item}:")
+# for i in range(1, 11):
+#     print(f"{i} * {item} = {i * item}")
+
+
+# a=0
+# b=1  #series=0,1,2,3,4
+# for i in range(2,5):
+#     c=a+b
+#     print(c)
+#     a,b=b,c
+
+# n=int(input("Enter the term- "))
+# a=0
+# b=1
+# count=0
+# while count<n:
+#     a,b=b,a+b
+#     count+=1
+#     print(f"FIBONACCI SERIES OF {n}=",a)
+
+#armstrong a number that is the sum of its own digits each raised to the power of the number of digits
+
+# num=int(input("Enter the number: "))
+# for i in range(1,num+1):
+#       sum=0
+#       temp=i
+#       count=0
+#       while temp!=0:
+#             count+=1
+#             temp//=10
+#             temp=i
+#             temp=i
+#             while temp!=0:
+#                   digit=temp%10
+#                   sum+=digit**count
+#                   temp//=10
+#                   if i==sum:
+#                         print(i,"is an armstrong number")
+#                         break
+
+
+
+
+
+
+
+    
+
+
+
+          
+        
+
+        
+
+
+
+
+
+
+
+
